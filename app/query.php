656e8c1b981086c71f19a5fc29474b9afe1cb4a2
Gregory Ewing <gregjewi@umich.edu>
2020-05-04 22:48:43 -0500

    Edit comments and filename
    Edited comments and filename to reflect new capabilities.

--- app/query.php
+++ app/query.php
@@ -1,14 +1,9 @@
 <?php
-
-// DESCRIPTION: query 1 days worth of data from the location supplied in the url.
-
+// DESCRIPTION: query n days worth of data from the site id supplied in the url.
 // Returns results as JSON in descending order from most recent data point.
-
 
 require __DIR__ . '/vendor/autoload.php';
 include __DIR__ . '/dbconnect.php';
-
-
 
 # Sensor ID + days sampled
 $sid = $_GET["sid"];
